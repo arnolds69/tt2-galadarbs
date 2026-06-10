537524dc16a88b3a69b95a119a4b094622086826
arnolds69 <berzins.arnolds@gmail.com>
2026-06-10 19:26:30 +0300

    database migrations add

---
+++ database/migrations/2026_06_10_155956_create_exercises_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+{
+    Schema::create('exercises', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('post_id')->constrained('posts');
+        $table->string('exercisename');
+        $table->unsignedInteger('duration')->nullable();
+        $table->decimal('weight', 6, 2)->nullable();
+        $table->text('description')->nullable();
+        $table->date('date')->nullable();
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('exercises');
+    }
+};
